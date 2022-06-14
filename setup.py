f0197df27675ff5b73bd5d191f269e0349abbf17
ionair <ionair.ironwood@gmail.com>
2022-06-14 18:54:36 +0800

    aaaaaa

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Install packages as defined in this file into the Python environment."""
+from setuptools import setup, find_packages
+
+setup(
+    name="prefillqr",
+    url="https://github.com/IonAir1/qrcode",
+    description="creates a qr code to a pre filled g forms",
+    version="0.0.1",
+    packages=find_packages(where=".", exclude=["tests"]),
+    install_requires=[
+        "setuptools>=45.0",
+        "bitlyshortener>=0.6.1",
+        "pandas>=1.4.2",
+        "Pillow>=9.1.1",
+    ],
+)
--- start.py
+++ start.py
@@ -27,6 +27,7 @@ else:
 
 if config.has_option('main','bitly_token'):
     bitly_token = config.get('main', 'bitly_token').split(",")
+    bitly_token = list(filter(None, bitly_token))
 else:
     bitly_token = []
     
@@ -48,89 +49,94 @@ print("enter \"help\" for more help")
 
 
 while run == True:
+    ans = ""
     ans = input("Prefilled G Forms QR Code Generator: ").split()
+    #print(str(ans) + " sfdsfsdfesfsdfds")
     
-    if ans[0] == "exit" or ans[0] == "quit":
-        run = False
-
-    elif ans[0] == "about":
-        print("\nPrefilled G Forms QR Code Generator is a program that generates a prefill link of a google form based on a specified excel file. It is then shortened through bitly and turned into a qr code\n")
-    
-    elif ans[0] == "run":
-        prefill.run(excel_file, forms_link, bitly_token, destination)
+    if len(ans) > 0:
         
-    elif ans[0] == "token":
-        if len(ans) < 2:
-            print(str(bitly_token).translate({ord(c): None for c in '][,'}))
-        else:
-            if ans[1] == "add":
-                if len(ans) > 2:
-                    for i in range(len(ans) - 2):
-                        if not ans[i+2] in bitly_token:
-                            bitly_token.append(ans[i + 2])
-                            save('bitly_token',bitly_token)
-                else:
-                    print("Invalid arguments, enter \"help\" for help")
-                
-            elif ans[1] == "remove":
-                if len(ans) > 2:
-                    for i in range(len(ans) - 2):
-                        if ans[i+2] in bitly_token:
-                            bitly_token.remove(ans[i + 2])
-                            save('bitly_token',bitly_token)
-                else:
-                    print("Invalid arguments, enter \"help\" for help")
-                
-            elif ans[1] == "clear":
-                    bitly_token = []
-                    save('bitly_token',bitly_token)
-
-    elif ans[0] == "excel":
-        if len(ans) == 1:
-            print(excel_file)
-        elif len(ans) == 2:
-            excel_file = ans[1]
-            save('excel_file', excel_file)
-        else:
-            print("Invalid arguments, enter \"help\" for help")
-            
-            
-    elif ans[0] == "destination":
-        if len(ans) == 1:
-            print(destination)
-        elif len(ans) == 2:
-            destination = ans[1]
-            save('destination', destination)
-        else:
-            print("Invalid arguments, enter \"help\" for help")
-                
-
     
-    elif ans[0] == "form":
-        if len(ans) == 1:
-            print(forms_link)
-        elif len(ans) == 2:
-            forms_link = ans[1]
-            save('forms_link', forms_link)
+        if ans[0] == "exit" or ans[0] == "quit":
+            run = False
+
+        elif ans[0] == "about":
+            print("\nPrefilled G Forms QR Code Generator is a program that generates a prefill link of a google form based on a specified excel file. It is then shortened through bitly and turned into a qr code\n")
+
+        elif ans[0] == "run":
+            prefill.run(excel_file, forms_link, bitly_token, destination)
+
+        elif ans[0] == "token":
+            if len(ans) < 2:
+                print(str(bitly_token).translate({ord(c): None for c in '][,'}))
+            else:
+                if ans[1] == "add":
+                    if len(ans) > 2:
+                        for i in range(len(ans) - 2):
+                            if not ans[i+2] in bitly_token:
+                                bitly_token.append(ans[i + 2])
+                                save('bitly_token',bitly_token)
+                    else:
+                        print("Invalid arguments, enter \"help\" for help")
+
+                elif ans[1] == "remove":
+                    if len(ans) > 2:
+                        for i in range(len(ans) - 2):
+                            if ans[i+2] in bitly_token:
+                                bitly_token.remove(ans[i + 2])
+                                save('bitly_token',bitly_token)
+                    else:
+                        print("Invalid arguments, enter \"help\" for help")
+
+                elif ans[1] == "clear":
+                        bitly_token = []
+                        save('bitly_token',bitly_token)
+
+        elif ans[0] == "excel":
+            if len(ans) == 1:
+                print(excel_file)
+            elif len(ans) == 2:
+                excel_file = ans[1]
+                save('excel_file', excel_file)
+            else:
+                print("Invalid arguments, enter \"help\" for help")
+
+
+        elif ans[0] == "destination":
+            if len(ans) == 1:
+                print(destination)
+            elif len(ans) == 2:
+                destination = ans[1]
+                save('destination', destination)
+            else:
+                print("Invalid arguments, enter \"help\" for help")
+
+
+
+        elif ans[0] == "form":
+            if len(ans) == 1:
+                print(forms_link)
+            elif len(ans) == 2:
+                forms_link = ans[1]
+                save('forms_link', forms_link)
+            else:
+                print("Invalid arguments, enter \"help\" for help")
+
+        elif ans[0] == "help":
+            print("""
+            'exit'  or  'quit'     exits the program
+            'about'                displays program info
+            'run'                  generates the qr code
+            'token'                lists all the tokens
+            'token add <token>'    adds the specified bitly token/s (you may add multiple tokens at a time)
+            'token remove <token>' removes the specified bitly token/s (you may remove multiple tokens at a time)
+            'token clear'          clears the tokens
+            'destination'          displays the current destination folder
+            'destination <path>'   changes the destination folder to the specified path
+            'form'                 displays the current google form prefill link
+            'form <path>'          changes the google form prefill link to the specified url
+            'excel'                displays the current selected excel file
+            'excel <path>'         changes the excel file to the specified path
+            """)
+
         else:
-            print("Invalid arguments, enter \"help\" for help")
-    
-    elif ans[0] == "help":
-        print("""
-        'exit'  or  'quit'     exits the program
-        'about'                displays program info
-        'run'                  generates the qr code
-        'token'                lists all the tokens
-        'token add <token>'    adds the specified bitly token/s (you may add multiple tokens at a time)
-        'token remove <token>' removes the specified bitly token/s (you may remove multiple tokens at a time)
-        'token clear'          clears the tokens
-        'destination'          displays the current destination folder
-        'destination <path>'   changes the destination folder to the specified path
-        'form'                 displays the current google form prefill link
-        'form <path>'          changes the google form prefill link to the specified url
-        'excel'                displays the current selected excel file
-        'excel <path>'         changes the excel file to the specified path
-        """)
-        
-    else:
-        print("unknown command")
+            print("unknown command")
